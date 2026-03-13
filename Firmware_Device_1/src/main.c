4ef6e3099ae7ce8d468b056cd43b90b1bb1f163e
brandt3 <brandt.konik@yahoo.com>
2026-03-13 16:24:07 -0500

    Added generation of pub and piv key to keys file in main.c

--- Firmware_Device_1/src/main.c
+++ Firmware_Device_1/src/main.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stddef.h>
+#include <unistd.h> // library to check existing files
+#include <openssl/pem.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+
 
 // Possible later note look into making it so only device_1 can open the keys and certs file within 
 // it's on directory and no one else can as that is a security threat
@@ -14,6 +20,60 @@ int main() {
     // 1. Upon "Power On" Check if there are keys created if not Create Private and Public key (RSA or EC)
         // Store Key in the keys file
         // Note Keys persist across reboots (try to emulate that here as well so only create the keys once)
+
+    // Store within the key files for security
+    char key_name_priv[35] = "keys/firmware1_priv_rsa_key.pem";
+    char key_name_pub[35] = "keys/firmware1_pub_rsa_key.pem";
+
+
+    // If keys doesn't exisit
+    if (access(key_name_priv, F_OK) != 0 && access(key_name_pub, F_OK) != 0) {
+        // Prepare area in memory for key generation
+        EVP_PKEY_CTX *ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
+
+        // Check for OpenSSL memory prepartion failure
+        if (ctx == NULL) {
+            fprintf(stderr, "Error preparing area in memory for Firmware Device 1 key creation\n");
+            ERR_print_errors_fp(stderr);
+            return 1;
+        }
+
+        // Initilize key generation at ctx location
+        EVP_PKEY_keygen_init(ctx);
+        // Set size of RSA key
+        EVP_PKEY_CTX_set_rsa_keygen_bits(ctx, 2048);
+        // Generate the key at pkey location
+        EVP_PKEY *pkey = NULL;
+        EVP_PKEY_keygen(ctx, &pkey);
+        // Free memory used for key generation
+        EVP_PKEY_CTX_free(ctx);
+
+        // Create file to store private key
+        FILE *file_storing_Privkey = fopen(key_name_priv, "wb");
+        // Make sure file opens smoothly
+        if (file_storing_Privkey == NULL) {
+            fprintf(stderr, "Error opening private key file\n");
+            return 1;
+        }
+        // There is a password arguement here could look into
+        PEM_write_PrivateKey(file_storing_Privkey, pkey, NULL, NULL, 0, NULL, NULL);
+        fclose(file_storing_Privkey);
+
+        // Write public key
+        FILE *file_storing_Pubkey = fopen(key_name_pub, "wb");
+        // Make sure file opens smoothly
+        if (file_storing_Pubkey == NULL) {
+            fprintf(stderr, "Error opening public key file\n");
+            return 1;
+        }
+        PEM_write_PUBKEY(file_storing_Pubkey, pkey);        
+        fclose(file_storing_Pubkey);
+
+
+        // Free memory storing key data structure as it is now written to a file a shouldn't be store to memory
+        EVP_PKEY_free(pkey);
+ 
+    }
 
     // 2. Create CSR and request user for CSR info plus exstention
         // Store CSR in certs file
